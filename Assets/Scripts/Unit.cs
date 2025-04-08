a2d20ef46447aab4d11ab4112b3c4aca2115ae95
Xtrife <Alucard.557@gmail.com>
2025-04-08 16:56:52 -0500

    add movement animation and rotation

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -2,16 +2,25 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator;
     private Vector3 targetPosition;
 
     private void Update()
     {
         float stopDistance = 0.1f; // Distance to stop moving towards the target position
+
+        // Check if the unit is close to the target position
         if (Vector3.Distance(transform.position, targetPosition) > stopDistance)
-        { // Check if the unit is close to the target position
+        { 
+            unitAnimator.SetBool("isWalking", true);
             Vector3 moveDirection = (targetPosition - transform.position).normalized; // Calculate the direction to the target position
             float moveSpeed = 4f;
+            float rotateSpeed = 15f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed); // Rotate the unit to face the target position
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        } else 
+        {
+            unitAnimator.SetBool("isWalking", false);
         }
 
         if (Input.GetMouseButtonDown(0))
